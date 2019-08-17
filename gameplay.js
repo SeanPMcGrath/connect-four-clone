439ea36d6c1aa318eba3b1fab8f39b69af7383eb
Sean McGrath <spmcgrath9@gmail.com>
2019-08-17 16:02:38 -0700

    initial pass at win check functions

--- gameplay.js
+++ gameplay.js
@@ -24,21 +24,121 @@ function gameTurn(currentBoard, player, playColumn) {
     return "Invalid move. Column is full";
   } else {
     currentBoard[playColumn][nextSpace] = player;
+    if (
+      winCheck(
+        currentBoard,
+        boardWidth,
+        boardHeight,
+        player,
+        playColumn,
+        nextSpace
+      )
+    ) {
+      return "WIN!";
+    }
     return currentBoard;
   }
 }
 
-let board = initializeArray(10, 3);
-console.log(board);
-board = gameTurn(board, 1, 3);
-console.log(board);
-board = gameTurn(board, 2, 7);
-console.log(board);
-board = gameTurn(board, 1, 7);
-console.log(board);
+function winCheck(gameBoard, boardWidth, boardHeight, player, column, row) {
+  //Supply 0-indexed column and row
+  //Supply 1-indexed boardWidth and boardHeight
+  boardWidth--;
+  boardHeight--;
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; i <= 1; j++) {
+      if (
+        !(i === 0 && j === 0) &&
+        validBoardPoint(boardWidth, boardHeight, column, row)
+      ) {
+        if (gameBoard[i][j] === player) {
+          //Check next location in that direction
+          const xChange = i - column;
+          const yChange = j - row;
+          if (
+            sequenceCheck(
+              gameBoard,
+              boardWidth,
+              boardHeight,
+              column + xChange,
+              row + yChange,
+              xChange,
+              yChange,
+              player,
+              sequenceCount
+            )
+          ) {
+            return true;
+          }
+        }
+      }
+    }
+  }
+  //If no win is found
+  return false;
+}
 
-board = gameTurn(board, 2, 7);
-console.log(board);
+function validBoardPoint(boardWidth, boardHeight, column, row) {
+  //All inputs 0-indexed
+  if (
+    column + i >= 0 &&
+    column + 1 < boardWidth &&
+    row + 1 >= 0 &&
+    row + 1 < boardHeight
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-board = gameTurn(board, 1, 7);
-console.log(board);
+function sequenceCheck(
+  gameBoard,
+  boardWidth,
+  boardHeight,
+  column,
+  row,
+  xChange,
+  yChange,
+  player,
+  sequenceCount
+) {
+  if (!validateBoardPoint(boardWidth, boardHeight, column, row)) {
+    return false;
+  } else if (gameBoard[column][row] === player) {
+    sequenceCount++;
+    if (sequenceCount === 4) {
+      return true;
+    } else {
+      //Check next spot in sequence
+      return sequenceCheck(
+        gameBoard,
+        boardWidth,
+        boardHeight,
+        column + xChange,
+        row + yChange,
+        xChange,
+        yChange,
+        player,
+        sequenceCount
+      );
+    }
+  } else {
+    return false;
+  }
+}
+
+// let board = initializeArray(10, 3);
+// console.log(board);
+// board = gameTurn(board, 1, 3);
+// console.log(board);
+// board = gameTurn(board, 2, 7);
+// console.log(board);
+// board = gameTurn(board, 1, 7);
+// console.log(board);
+
+// board = gameTurn(board, 2, 7);
+// console.log(board);
+
+// board = gameTurn(board, 1, 7);
+// console.log(board);
